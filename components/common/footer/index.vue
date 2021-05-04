fd52e79a17975fa5eac5424cb7b3b5a73f8988fe
Incipient Info <info@incipientinfo.com>
2021-05-04 19:24:10 +0530

    Initial commit

---
+++ components/common/footer/index.vue
@@ -0,0 +1,172 @@
+<template>
+  <div>
+    <!-- main section -->
+    <div class="footer">
+      <div class="d-flex flex-column">
+        <div class="home__logo text-center">
+          <div class="text-center">
+            <div class="mb-3">
+              <img alt="" class="img-fluid lazyLoad isLoaded" :src="imgUrl" />
+            </div>
+            <div>
+              <img src="@/assets/images/logoline.f940fa9.png" alt="Image" />
+            </div>
+            <p class="title mt-3 pt-1 mb-3">{{ title }}</p>
+            <!-- page links -->
+            <div class="pt-40">
+              <div
+                align-v="center"
+                class="d-flex justify-content-center align-self-center"
+              >
+                <div class="list-group list-group-horizontal">
+                  <a
+                    href="/#"
+                    class="btn d-flex align-self-center link text-uppercase"
+                    v-for="(pageLink, index) in pageLinks"
+                    :key="index"
+                  >
+                    <div class="list-group-item bg-transparent border-0">
+                      {{ pageLink.title }}
+                    </div>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <!-- //legal links -->
+            <div>
+              <div
+                align-v="center"
+                class="d-flex justify-content-center align-self-center"
+              >
+                <div class="list-group list-group-horizontal">
+                  <a
+                    href="/#"
+                    class="btn d-flex align-self-center link"
+                    v-for="(legalLink, index) in legalLinks"
+                    :key="index"
+                  >
+                    <div class="list-group-item bg-transparent border-0 p-0">
+                      {{ legalLink.title }}
+                    </div>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- social div -->
+    <div class="social_parent">
+      <div class="container">
+        <b-row>
+          <div class="col-lg-4 text-center">
+            <a href="tel:+436763893322" class="social_meta">
+              <img
+                src="https://admin.m2-wunschauto.at/wp-content/uploads/2021/02/phone-call.png"
+              />
+              <p class="mt-3">0676 38 93 322</p>
+            </a>
+          </div>
+          <div class="col-lg-4 text-center">
+            <a href="mailto:office@m2-wunschauto.at" class="social_meta">
+              <img
+                src="https://admin.m2-wunschauto.at/wp-content/uploads/2021/02/email.png"
+              />
+              <p class="mt-3">office@m2-wunschauto.at</p>
+            </a>
+          </div>
+          <div class="col-lg-4 text-center">
+            <a
+              target="_blank"
+              href="https://www.instagram.com/m2.wunschauto/"
+              class="social_meta"
+            >
+              <img
+                src="https://admin.m2-wunschauto.at/wp-content/uploads/2021/02/instagram.png"
+              />
+              <p class="mt-3">Instagram</p>
+            </a>
+          </div>
+        </b-row>
+      </div>
+    </div>
+    <!-- copyright -->
+    <div class="copyright text-center">
+      <h6 class="copyright_text">
+        ©copyright 2021 M2-wunschauto . All Rights Reserved
+      </h6>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: ["footerDataProp"],
+  data() {
+    return {
+      imgUrl: this.footerDataProp.imgUrl,
+      title: this.footerDataProp.title,
+      pageLinks: this.footerDataProp.pageLinks,
+      legalLinks: this.footerDataProp.legalLinks
+    };
+  },
+  watch: {
+    footerDataProp: {
+      deep: true,
+      handler(newVal, olVal) {
+        this.imgUrl = newVal.imgUrl;
+        this.title = newVal.title;
+        this.pageLinks = newVal.pageLinks;
+        this.legalLinks = newVal.legalLinks;
+      }
+    }
+  }
+};
+</script>
+<style scoped>
+.title {
+  color: #182540;
+  font-family: Open Sans, sans-serif !important;
+  font-size: 1.0625rem;
+}
+.link {
+  font-family: Open Sans, sans-serif !important;
+  font-weight: 600;
+  color: #182540;
+}
+.social_parent {
+  background: #f8f9fa;
+  padding-top: 4rem;
+  padding-bottom: 3rem;
+}
+.social_meta:hover,
+.social_meta {
+  color: #030508;
+  text-decoration: none;
+}
+.copyright_text {
+  font-family: Open Sans, sans-serif !important;
+  font-size: 0.875rem;
+  color: #fff;
+  margin-bottom:0px;
+}
+.copyright {
+  background-color: #182540;
+    padding-top: 11px;
+    padding-bottom: 10px;
+}
+.pt-40{
+	padding-top:40px;
+	padding-bottom:12px;
+}
+.footer a{
+	padding:0px!important;
+}
+.footer{
+	padding:100px 0px;
+}
+.footer .list-group-item{
+	padding:0px 25px !important;
+	font-size:17px;
+}
+</style>
---
+++ components/home/sections/section_services/index.vue
@@ -0,0 +1,166 @@
+<template>
+  <div>
+    <b-row v-for="(service, index, count) in services" :key="index" class="m-0">
+      <!-- For first record: display image in right, for second record: display image in left & continue same pattern -->
+      <!-- image in right -->
+      <b-col lg="6" class="d-none d-md-block p-0 " v-if="count % 2 != 0">
+        <img :src="service.image.url" class="w-100 fluid image h-100" />
+      </b-col>
+      <b-col
+        lg="6"
+        sm="12"
+        class="p-0 padd_left_12_per pt-5  col-sm-12 col-lg-6"
+      >
+        <h2 class="pb-3 title">{{ service.title }}</h2>
+        <div v-for="(list, index) in service.list" :key="index">
+          <b-img src="@/assets/images/tick.e546d3c.svg" class="float-left">
+          </b-img>
+          <p class="list_title pb-3 ml-2">{{ list.title }}</p>
+        </div>
+        <div
+          class=" mb-2  d-flex flex-sm-row justify-content-center justify-content-sm-start flex-column w-100 pt-24"
+        >
+          <div class="w-50">
+            <a href="#" class="btn-red button btn btn-red-expand">
+              {{ service.button }}
+            </a>
+          </div>
+          <div
+            class="d-flex align-items-start flex-column bd-highlight "
+            v-if="service.price"
+          >
+            <div class="bd-highlight">
+              <div class="price_tag">
+                <b-img src="@/assets/images/price_tag.jpeg"></b-img>
+                <p class="price_tag_text">PRIES</p>
+              </div>
+              <h5 class="price">
+                {{ service.price }}
+              </h5>
+            </div>
+          </div>
+        </div>
+      </b-col>
+      <!-- image in left -->
+      <b-col lg="6" class="d-none d-md-block p-0 h-652" v-if="count % 2 == 0">
+        <img :src="service.image.url" class="w-100 fluid h-100" />
+      </b-col>
+    </b-row>
+  </div>
+</template>
+<script>
+export default {
+  props: ["servicesProp"],
+  data() {
+    return {
+      count: 0,
+      services: this.servicesProp
+    };
+  },
+  watch: {
+    servicesProp: {
+      deep: true,
+      handler(newVal, oldVal) {
+        this.services = newVal;
+      }
+    }
+  }
+};
+</script>
+<style scoped>
+.padd_left_12_per{
+  padding-left: 64px!important;
+  padding-top:80px!important;
+  padding-bottom:72px!important;
+}
+.row.m-0:first-child .padd_left_12_per{
+  padding-left: 14.4% !important;
+}
+
+.list_title {
+    font-size: 1.0625rem;
+    color: #182540;
+    padding-left: 41px;
+    font-family: Open Sans,sans-serif!important;
+    font-size: 1.0625rem;
+    color: #182540;
+    margin-bottom: 0px;
+    padding-bottom: 36px!important;
+}
+.image {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  -o-object-fit: cover;
+  object-fit: cover;
+}
+.title {
+  font-family: Lora, serif !important;
+  font-size: 2.625rem;
+  color: #182540;
+  font-weight: 500;
+  padding-bottom: 20px !important;
+}
+.price_tag {
+  position: relative;
+}
+.price_tag_text {
+  position: absolute;
+  top: 1px;
+  left: 10px;
+  color: #fff;
+  letter-spacing: 7px;
+  font-family: Open Sans, sans-serif !important;
+  font-size: 0.875rem;
+}
+.price {
+  font-family: Lora, serif !important;
+  font-size: 2.625rem;
+  font-weight: 500;
+  color: #182540;
+}
+.pt-24{
+	padding-top:24px;
+}
+a.btn-red.button.btn.btn-red-expand {
+    border-radius: 60px !important;
+    background: linear-gradient(90deg, #e50612, #730309) !important;
+    color: #fff !important;
+    text-align: center !important;
+    padding: 14px 12px 6px 12px;
+    border: 0;
+    overflow: hidden;
+    position: relative;
+    z-index: 1;
+    font-family: Open Sans, sans-serif !important;
+    font-size: 18px;
+    line-height: 27px;
+    height: 59px;
+}
+.w-50{
+	flex-basis:50%
+}
+.btn-red-expand:hover {
+    transition: all .5s!important;
+    width: 262px;
+    letter-spacing: 2px;
+}
+.d-none.d-md-block.p-0.h-652.col-lg-6{
+	position:relative;
+}
+.h-652 img.w-100.fluid.h-100 {
+    object-fit: cover;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    -o-object-fit: cover;
+    object-fit: cover;
+}
+.btn-red-expand {
+  width: 209px;
+}
+</style>
